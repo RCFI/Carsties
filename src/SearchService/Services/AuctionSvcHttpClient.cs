1a8173f5729255f4ff4002287fe6932a201d4b76
bessilen <bessilen@gmail.com>
2024-02-01 19:37:35 +0100

    sync search with auction service

---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services;
+
+public class AuctionSvcHttpClient(HttpClient client, IConfiguration configuration)
+{
+    private readonly HttpClient _client = client ?? throw new ArgumentNullException(nameof(client));
+
+    private readonly IConfiguration _configuration =
+        configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>().Sort(x => x
+                .Descending(i => i.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+        
+        return await _client.GetFromJsonAsync<List<Item>>($"{_configuration["AuctionServiceUrl"]}/api/auctions?date={lastUpdated}");
+    }
+}

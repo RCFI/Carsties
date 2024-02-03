884d0116411173f64809a8aab7bf92067ce9a010
bessilen <bessilen@gmail.com>
2024-02-03 20:47:41 +0100

    dockerize search-svc

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -19,6 +19,11 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, configurator) =>
     {
+        configurator.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
         configurator.ReceiveEndpoint("search-auction-created", e =>
         {
             e.UseMessageRetry(r=> r.Interval(5, TimeSpan.FromSeconds(5)));
